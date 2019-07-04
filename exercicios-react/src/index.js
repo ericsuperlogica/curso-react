e5cc76215c2c9e283912e04f97d391237ce49a6b
eric.superlogica <eric.strumendo@superlogica.com>
2019-07-03 22:59:11 -0300

    Introdução Finalizada - Aula 33

--- exercicios-react/src/index.js
+++ exercicios-react/src/index.js
@@ -4,17 +4,27 @@ import ReactDOM from 'react-dom'
 // import  CompA, { CompB as B}  from './componentes/DoisComponenentes'
 // import MultiElementos from './componentes/MultiElementos'
 // import FamiliaSilva from './componentes/FamiliaSilva'
-import Familia from './componentes/Familia'
-import Membro from './componentes/Membro'
+// import Familia from './componentes/Familia'
+// import Membro from './componentes/Membro'
+// import ComponenteComFuncao from './componentes/ComponenteComFuncao'
+// import Pai from './componentes/Pai'
+// import ComponenteClasse from './componentes/ComponenteClasse'
+// import Contador from './componentes/Contador'
+import Hook from './componentes/Hook'
 
 const elemento = document.getElementById('root')
 
 ReactDOM.render(
     <div>
-        <Familia sobrenome="Pereira">
+        <Hook />
+        {/* <Contador numeroInicial={100} /> */}
+        {/* <ComponenteClasse valor='Eric' /> */}
+        {/* <Pai /> */}
+        {/* <ComponenteComFuncao /> */}
+        {/* <Familia sobrenome="Pereira">
             <Membro nome="André" />
             <Membro nome="Mariana" />
-        </Familia>
+        </Familia> */}
 
         {/* <MultiElementos /> */}
         {/* <PrimeiroComponente valor='Eric'/> */}
